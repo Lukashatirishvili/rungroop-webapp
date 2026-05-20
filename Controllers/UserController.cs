56453b5af5fc6072684fb486249908cf57596b58
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-05-20 17:27:42 +0400

    Added List/Detail Users

---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Interfaces;
+using RunGroopWebApp.ViewModels;
+
+namespace RunGroopWebApp.Controllers;
+
+public class UserController : Controller
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    [HttpGet("Users")]
+    public async Task<IActionResult> Index()
+    {
+        var users = await _userRepository.GetAllUsers();
+        
+        List<UserViewModel> result = new List<UserViewModel>();
+
+        foreach (var user in users)
+        {
+            var userViewModel = new UserViewModel
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Pace = user.Pace,
+                Mileage = user.Mileage
+            };
+            result.Add(userViewModel);
+        }
+        
+        return View(result);
+    }
+
+    public async Task<IActionResult> Detail(string id)
+    {
+        var detail = await _userRepository.GetUserById(id);
+
+        var userDetailViewModel = new UserDetailViewModel
+        {
+            Id = id,
+            UserName = detail.UserName,
+            Pace = detail.Pace,
+            Mileage = detail.Mileage,
+        };
+        
+        return View(userDetailViewModel);
+    }
+}
