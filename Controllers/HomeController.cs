e47c1714e0382af44cbacbefeb865574d41e2d66
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-05-22 13:59:01 +0400

    Added IP Geolocation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,54 @@
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RunGroopWebApp.Helpers;
+using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
+using RunGroopWebApp.ViewModels;
 
 namespace RunGroopWebApp.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly IClubRepository _clubRepository;
+
+    public HomeController(IClubRepository clubRepository)
     {
-        return View();
+        _clubRepository = clubRepository;
+    }
+    public async Task<IActionResult> Index()
+    {
+        
+        var ipInfo = new IPInfo();
+        var homeViewModel = new HomeViewModel();
+
+        try
+        {
+            string url = "https://api.ipinfo.io?token=68ae1fa46d2d10";
+            var info = new WebClient().DownloadString(url);
+            ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+            RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+            ipInfo.Country = myRI1.EnglishName;
+            homeViewModel.City = ipInfo.City;
+            homeViewModel.State = ipInfo.Region;
+
+            if (homeViewModel.City == null)
+            {
+                homeViewModel.Clubs = await _clubRepository.GetClubByCity(homeViewModel.City);
+            }
+            else
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
+        }
+        catch (Exception ex)
+        {
+            homeViewModel.Clubs = null;
+        }
+        
+        return View(homeViewModel);
     }
 }
